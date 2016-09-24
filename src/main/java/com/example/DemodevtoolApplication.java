ebbf7a88176c6cef2de9b2774c22fe07b7bd0c5d
eddhie <eddy_ruslim@yahoo.com>
2016-09-24 21:50:29 +0700

    Initial commit

---
+++ src/main/java/com/example/DemodevtoolApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodevtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodevtoolApplication.class, args);
+	}
+}
